3a817c45658fb90a7b8977aae5dda85dca1acae0
Carlos Heitor <carlosheitorjesus80@gmail.com>
2026-05-21 17:05:45 -0300

    Add Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,41 +1,14 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-import db.DB;
-import db.DbException;
+import model.entities.Departmnet;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Connection conn = null;
-		PreparedStatement st = null;
-		try {
-			conn = DB.getConnection();
-			
-			st = conn.prepareStatement(
-					"DELETE FROM department "
-					+ "WHERE "
-					+ "Id = ?");
-			
-			st.setInt(1, 2);
-			
-			int rowsAffected = st.executeUpdate();
-			
-			System.out.println("Done! Rows affected: " + rowsAffected);
-					
-		}
-		catch (SQLException e) {
-			throw new DbException(e.getMessage());
-		}
-		finally {
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
-		
+		Departmnet obj = new Departmnet(1, "Books");
+		System.out.println(obj);
 
 	}
+
 }
